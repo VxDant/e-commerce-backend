1651117a160c01d470eb84ab4dd4d9f99373e6de
Vedant Achtani <72167208+VxDant@users.noreply.github.com>
2024-07-18 23:56:49 +0200

    setting backend repo

---
+++ src/main/java/com/e_commerce/e_bay/EBayApplication.java
@@ -0,0 +1,13 @@
+package com.e_commerce.e_bay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e_commerce/e_bay/EBayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.e_bay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
